9cdd84738cef10a15b01a83baae3114ded836280
billchetch <45006134+billchetch@users.noreply.github.com>
2025-02-01 22:36:58 +0700

    sdfsd

--- ArduinoTest/Program.cs
+++ ArduinoTest/Program.cs
@@ -8,19 +8,14 @@ namespace ArduinoTest;
 
 class Program
 {
-    static async void foo(){
-        Task t = Task.Run(()=>{
-
-            throw new Exception("Fuck this");
-        });
-        try{
-            await t;
-        } catch (AggregateException e){
-            Console.WriteLine("Some bullshit");
-        }
+    static void printStatus(ArduinoBoard board)
+    {
+        Console.WriteLine("Board millis: {0}", board. Millis);
+        Console.WriteLine("Devices: {0}", board. DeviceCount);
+        Console.WriteLine("Free memory: {0}", board.FreeMemory);
     }
 
-    static void Main(string[] args)
+    static async Task Main(string[] args)
     {
         //ConsoleHelper.PK("Press a key to start");
        
@@ -30,27 +25,45 @@ class Program
         ArduinoBoard board = new ArduinoBoard("first", 0x7523, 9600); //, Frame.FrameSchema.SMALL_NO_CHECKSUM);
         board.Ready += (sender, ready) => {
             Console.WriteLine("Board is ready: {0}", ready);
-            Console.WriteLine("Free memory: {0}", board.FreeMemory);
+            if(ready)printStatus(board);
         };
 
-        board.MessageReceived += (sender, message) => {
-            Console.WriteLine("Message received!");
+        board.ErrorReceived += (sender, eargs) => {
+            Console.WriteLine("{0} resulted in an error {1}", sender, eargs.Error);
+            if(eargs.Error == ArduinoBoard.ErrorCode.DEVICE_ERROR && eargs.ErrorSource != null)
+            {
+                Console.WriteLine("Specific error is: {0}", ((ArduinoDevice)eargs.ErrorSource).Error);
+            }
         };
 
-        var device = new Ticker("testDevice01");
-        device.Updated += (sender, properties) => {
-            Console.WriteLine(device.Count);
+        //var ticker = new Ticker("testDevice01");
+        var switchDevice = new SwitchDevice("glob");
+        switchDevice.Switched += (sender, pinState) => {
+            Console.WriteLine("{0} has pin state {1}", switchDevice.Name, switchDevice.PinState);
         };
+
+        //board.AddDevice(ticker);
+        board.AddDevice(switchDevice);
+
+        await Task.Run(()=>{
+            try{
+                board.Begin();
+            } catch (Exception e)
+            {
+                Console.WriteLine("FucK: {0}", e.Message);
+            }
+            while(!board.IsReady)
+            {
+                Console.WriteLine("Waiting for board to become ready...");
+                Thread.Sleep(500); 
+            }
+        });
         
-        board.AddDevice(device);
-        try
-        {
-            board.Begin();
-        }
-        catch(Exception e)
-        {
-            Console.WriteLine(e.Message);
-        }
+        ConsoleHelper.PK("Press a key to send a test thingy");
+        switchDevice.TurnOn();
+
+        ConsoleHelper.PK("Press a key to send a test thingy");
+        switchDevice.TurnOff();
 
         ConsoleHelper.PK("Press a key to disconnect");
         board.End();
