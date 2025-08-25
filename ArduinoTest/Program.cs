5438482d6eb42881dfec5e843d8b8125df1753ec
bill@chetch.net <bill@chetch.net>
2025-08-25 22:51:36 +0800

    asdf

--- ArduinoTest/Program.cs
+++ ArduinoTest/Program.cs
@@ -5,23 +5,31 @@ using Chetch.Utilities;
 using Chetch.Arduino.Devices;
 using Chetch.Arduino.Devices.Displays;
 using System.Threading.Tasks;
+using Chetch.Arduino.Boards;
+using Chetch.Arduino.Connections;
 
 namespace ArduinoTest;
 
 class Program
 {
     const int BAUDRATE = 9600;
-    const string PATH2DEVICE = "/dev/tty.usb*";
+    const string MAC_PATH2DEVICE = "/dev/tty.usb*";
+    const string LINUX_PATH2DEVICE = "/dev/serial/by-id/usb-1a86*";
+
+    static String getPath2Device()
+    {
+        if (OperatingSystem.IsLinux())
+        {
+            return LINUX_PATH2DEVICE;
+        }
+        else if (OperatingSystem.IsMacOS())
+        {
+            return MAC_PATH2DEVICE;
+        }
+        throw new Exception("Cannot provide path 2 device");
+        
+    }
     //const string PATH2DEVICE = "/dev/cu.usb*";
-
-    const byte OLED_DEVICE_ID = 10;
-    const byte RECEIVER_ID = 11;
-    const byte LGINSIDE_ID = 12;
-    const byte LGOUTSIDE_ID = 13;
-
-
-    static OLEDTextDisplay oled = new OLEDTextDisplay(OLED_DEVICE_ID, "oled");
-    static IRTransmitter lgInside = new IRTransmitter(LGINSIDE_ID, "inside");
 
     static void printStatus(ArduinoBoard board)
     {
@@ -30,63 +38,17 @@ class Program
         Console.WriteLine("Free memory: {0}", board.FreeMemory);
     }
 
-
-    static int n = 0;
-    static async Task<int> DoCountAsync(bool thro)
-    {
-        if(thro)throw new Exception("wow");
-        
-        Task t = Task.Run(() =>{
-            n = 0;
-            for (int i = 1; i <= 5; i++)
-            {
-                n = i;
-                //Console.WriteLine("Count: {0}", n);
-                Thread.Sleep(500);
-            }
-            
-        });
-        
-        await t;
-        return n;
-    }
-
-    
-    static async Task<Dictionary<String, IRData>> GetDeviceCommands(String deviceName)
-    {
-        String filename = "commands-" + deviceName.ToLower().Replace(" ", "-") + ".json";
-        String content = String.Empty;
-        if (File.Exists(filename))
-        {
-            content = File.ReadAllText(filename);
-        }
-        else
-        {
-            String uri = String.Format("http://localhost:8008/api/device-commands?device={0}", deviceName);
-            using (HttpClient client = new HttpClient())
-            {
-                var response = await client.GetAsync(uri);
-                content = await response.Content.ReadAsStringAsync();
-            }
-            if (!String.IsNullOrEmpty(content))
-            {
-                File.WriteAllText(filename, content);
-            }
-        }
-        var result = JsonSerializer.Deserialize<List<IRData>>(content);
-        Dictionary<String, IRData> commands = result.ToDictionary(x => x.CommandAlias, x => x);
-        return commands;
-    }
-
-
     static async Task Main(string[] args)
     {
         //ConsoleHelper.PK("Press a key to start");
-       
+
         //foo();
 
         //ArduinoBoard board = new ArduinoBoard(0x0043, 9600, Frame.FrameSchema.SMALL_NO_CHECKSUM);
-        ArduinoBoard board = new ArduinoBoard("first", 0x7523, 9600); //, Frame.FrameSchema.SMALL_NO_CHECKSUM);
+        //ArduinoBoard board = new ArduinoBoard("first", 0x7523, 9600); //, Frame.FrameSchema.SMALL_NO_CHECKSUM);
+        CANBusMonitor board = new CANBusMonitor(2);
+        board.Connection = new ArduinoSerialConnection(getPath2Device(), BAUDRATE);
+
         board.Ready += (sender, ready) => {
             Console.WriteLine("Board is ready: {0}", ready);
             if (ready) printStatus(board);
@@ -100,42 +62,28 @@ class Program
             }
         };
 
-        var ticker = new Ticker(10, "testDevice01");
-        ticker.Updated += (sender, props) =>{
-            Console.WriteLine("Ticker count is: {0}", ticker.Count);
-        };
-        //board.AddDevice(ticker);
-        
-        var switchDevice = new SwitchDevice(11, "glob");
-        switchDevice.Switched += (sender, pinState) => {
-            Console.WriteLine("{0} has pin state {1}", switchDevice.Name, switchDevice.PinState);
-        };
-        board.AddDevice(switchDevice);
-
         //ConsoleHelper.PK("Press a key to begin");
+        ConsoleHelper.CLRLF();
         
-        await Task.Run(()=>{
-            try{
+        await Task.Run(() =>
+        {
+            try
+            {
                 board.Begin();
-            } catch (Exception e)
+            }
+            catch (Exception e)
             {
                 Console.WriteLine("FucK: {0}", e.Message);
             }
 
-            Thread.Sleep(1000); 
-            while(!board.IsReady)
+            Thread.Sleep(1000);
+            while (!board.IsReady)
             {
                 Console.WriteLine("Waiting for board to become ready...");
                 Thread.Sleep(3000);
             }
         });
         
-        ConsoleHelper.PK("Press a key to send a test thingy");
-        switchDevice.TurnOn();
-
-        ConsoleHelper.PK("Press a key to send a test thingy");
-        switchDevice.TurnOff();
-
         ConsoleHelper.PK("Press a key to disconnect");
         board.End();
 
